9bd8bafe09b8fd3a8fb1054167ca55d8077dbed6
cqw <2513337026@qq.com>
2021-11-09 19:57:47 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,25 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    console.log(options.url);
+        // console.log(options.url);
+
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = function(res) {
+        // console.log(res);
+        // console.log('执行回调');
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+                // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
